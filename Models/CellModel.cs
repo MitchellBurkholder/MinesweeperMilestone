b0ea1de7853771dddb950c9443934a4a8f2b7c1b
Jackson Jens <tokenlaptop@live.com>
2026-04-10 15:41:54 -0700

    Added CellModel.cs

---
+++ Models/CellModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MinesweeperMilestone.Models
+{
+    public class CellModel
+    {
+        public int Id { get; set; }
+        public int CellState { get; set; }
+        public string CellImage { get; set; }
+
+        public CellModel(int id, int cellState, string cellImage)
+        {
+            Id = id;
+            CellState = cellState;
+            CellImage = cellImage;
+        }
+
+        public CellModel()
+        {
+
+        }
+    }
+}
