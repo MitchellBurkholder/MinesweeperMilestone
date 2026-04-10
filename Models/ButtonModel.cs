d793e36e1ea539c7d217ac80e2496ae519b7ed08
Jackson Jens <tokenlaptop@live.com>
2026-04-10 15:47:16 -0700

    Added ButtonController.cs, renamed CellModel.cs to ButtonModel.cs

---
+++ Models/ButtonModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MinesweeperMilestone.Models
+{
+    public class ButtonModel
+    {
+        public int Id { get; set; }
+        public int ButtonState { get; set; }
+        public string ButtonImage { get; set; }
+
+        public ButtonModel(int id, int buttonState, string buttonImage)
+        {
+            Id = id;
+            ButtonState = buttonState;
+            ButtonImage = buttonImage;
+        }
+
+        public ButtonModel()
+        {
+
+        }
+    }
+}
