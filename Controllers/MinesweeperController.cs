c9b47dd0f31d416085baca591b19ad2dd4385cd9
Jackson Jens <tokenlaptop@live.com>
2026-04-11 13:09:26 -0700

    Created MinesweeperController.cs to initialize the board

---
+++ Controllers/MinesweeperController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinesweeperMilestone.Models;
+
+namespace MinesweeperMilestone.Controllers
+{
+    public class MinesweeperController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Initializes a 10x10 board on Easy (Difficulty 1)
+            Board gameBoard = new Board(10, 1);
+
+            return View(gameBoard);
+        }
+    }
+}
