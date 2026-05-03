bd5d43d3991ff99574ea864f2f5a99216be14a39
MitchellBurkholderGCU <stinkbugranger@proton.me>
2026-05-02 20:01:34 -0400

    basis of rest API  stuff, will see if more crud ops will be needed if data base can't be accessed.

---
+++ Controllers/SaveGameRestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinesweeperMilestone.Controllers
+{
+    [ApiController]
+    [Route("api/showSavedGames")]
+    public class SaveGameRestController : ControllerBase
+    {
+        
+    }
+}
